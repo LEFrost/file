caa5ea5f414e6cad88a94eabfbcc29f1dd53670d
建 彭 <pengjian@live.cn>
2016-05-11 12:24:45 +0800

    添加项目文件。

---
+++ file/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace file
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const string filename = "storage";
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            await FileIO.WriteTextAsync(file, input.Text);
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var fold = ApplicationData.Current.LocalFolder;
+            StorageFile file = await fold.GetFileAsync(filename);
+            output.Text = await FileIO.ReadTextAsync(file);
+        }
+    }
+}
